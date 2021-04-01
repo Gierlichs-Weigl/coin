f013c50890764104a4542c6e311a9fdd1dfc6e2f
florianGierlichs <gierlichs@gmx.de>
2021-04-01 12:09:09 +0200

    Remove comments

--- server/routes/user.ts
+++ server/routes/user.ts
@@ -1,23 +1,9 @@
-import {Router} from 'express';
-// import { ExistingUser } from '../../interfaces';
-// import User from '../models/User';
-// import { hash, compare } from 'bcrypt';
-// import { sign } from 'jsonwebtoken';
-// import cookie from 'cookie';
+import { Router } from 'express';
 
 const userRoutes = Router();
 
-
-//
-// -- get --
-//
-
-
 userRoutes.get('/:user_id', async (_, res) => {
-        res.send({message: 'HELLOWorld'}).json()
+  res.send({ message: 'HELLOWorld' }).json();
 });
 
-
-
-
-export default userRoutes
+export default userRoutes;
