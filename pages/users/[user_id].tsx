47e3941754b597b452ba5e1bd1689d9b7034183d
florianGierlichs <gierlichs@gmx.de>
2021-04-03 16:54:08 +0200

    Add profile page

--- pages/users/[user_id].tsx
+++ pages/users/[user_id].tsx
@@ -1,14 +1,29 @@
+import Profile from 'components/Profile';
 import { GetServerSideProps } from 'next';
+import { RandomUser } from '@/interfaces/index';
+import Bio from 'components/Bio';
+import Contact from 'components/Contact';
 
 interface Props {
   currentUserData?: { message: string };
   userID: string;
+  randomUserData: RandomUser;
 }
 
-const UserProfile = ({ currentUserData, userID }: Props): JSX.Element => {
-  console.log(currentUserData);
-
-  return <div>{userID}</div>;
+const UserProfile: React.FC<Props> = ({ currentUserData, userID, randomUserData }): JSX.Element => {
+  const loggedIn = true;
+  return (
+    <>
+      <Profile randomUserData={randomUserData} />
+      <Bio />
+      {loggedIn && <Contact />}
+      <br />
+      <br />
+      <div>
+        {userID} {currentUserData?.message}
+      </div>
+    </>
+  );
 };
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
@@ -18,8 +33,11 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   const currentUserData = await currentUser.json();
   const userID = params?.user_id;
 
+  const randomUser = await fetch(`https://randomuser.me/api/`);
+  const randomUserData = await randomUser.json();
+
   return {
-    props: { currentUserData, userID },
+    props: { currentUserData, userID, randomUserData },
   };
 };
 
