bcfeb03a5fe3710a11b61537cd4dde346ef28999
florianGierlichs <gierlichs@gmx.de>
2021-04-05 13:25:09 +0200

    Remove dynamic header import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,19 @@
 import * as React from 'react';
-import dynamic from 'next/dynamic';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 
 import '../styles/globals.css';
-
-const DynamicHeader = dynamic(() => import('components/Header'), { ssr: false });
+import Header from 'components/Header';
 
 export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  // TODO: header render on server-side leads to a content missmatch warning
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Constellators International</title>
       </Head>
-      <DynamicHeader />
+      <Header />
       <div className="mx-2 my-2">
         <Component {...pageProps} />
       </div>
